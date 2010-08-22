3d6a5c670c38f6898744051a8b79f62e4a19a4ee
Tyler Flint <tylerflint@Delorum10.local>
2010-08-22 14:40:49 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBench::Application.config.secret_token = 'b39b5837ec3e8b6aeec23d039d7cd770f38a59871d434c6774f2af9aa3f0f60a64a17a0cb287e1b60bd6b24e666d79a27198f5f63e4cb6fecf268e44d32f77d8'
